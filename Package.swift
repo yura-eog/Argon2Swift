613f308c384af5da477b1916ff930b38616b0b5e
Yura Filatov <yfilatov@eogresources.com>
2023-11-01 16:39:38 -0500

    Stable version based on a forked PHC repo.

--- Package.swift
+++ Package.swift
@@ -1,34 +1,23 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Argon2Swift",
-    platforms: [.macOS(.v10_10), .tvOS(.v9), .iOS(.v9), .watchOS(.v2)],
+    platforms: [
+        .macOS(.v10_10), .tvOS(.v9), .iOS(.v9), .watchOS(.v2)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Argon2Swift",
-            targets: ["Argon2Swift"]),
+        .library(name: "Argon2Swift", targets: ["Argon2Swift"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(
-            name: "argon2", 
-            url: "https://github.com/P-H-C/phc-winner-argon2.git", .branch("master")
-        )
+        .package(name: "argon2", url: "https://github.com/yura-eog/phc-winner-argon2", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Argon2Swift",
-            dependencies: ["argon2"],
-            path: "Sources/Swift"),
-        .testTarget(
-            name: "Argon2SwiftTests",
-            dependencies: ["Argon2Swift"],
-            path: "Tests"),
+        .target(name: "Argon2Swift", dependencies: ["argon2"], path: "Sources/Swift"),
+        .testTarget(name: "Argon2SwiftTests", dependencies: ["Argon2Swift"], path: "Tests"),
     ]
 )
